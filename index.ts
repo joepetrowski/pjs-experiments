71878ea5743ab1c2e4c65216267a24b5c41df54a
joepetrowski <joe@parity.io>
2019-11-28 21:07:54 +0100

    some handy formatting functions

--- index.ts
+++ index.ts
@@ -8,11 +8,33 @@ $ yarn start
 
 // Import
 import { ApiPromise, WsProvider } from '@polkadot/api';
-// import { u64 } from '@polkadot/types';
 
-// Construct ws://127.0.0.1:9944
+// Construct
+// Local node: ws://127.0.0.1:9944
+// Parity node: wss://kusama-rpc.polkadot.io/
 const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io/');
 const DOT_DECIMAL = 1_000_000_000_000;
+
+// Format a number to display with commas every three digits.
+// For example, 1234567 becomes 1,234,567.
+function formatWithCommas(num: number) {
+	return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+// Convert a Unix timestamp to a string of "YYYY-month-DD HH:MM:SS".
+function timeConverter(unix: number) {
+	let date = new Date(unix);
+	let year = date.getFullYear();
+	let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	let month = months[date.getMonth()];
+	let day = date.getDate();
+	let hour = date.getHours();
+	let minute = date.getMinutes();
+	let second = date.getSeconds();
+	let datestring = year.toString() + '-' + month + '-' + day.toString() +
+		' ' + hour.toString() + ':' + minute.toString() + ':' + second.toString();
+	return datestring;
+}
 
 async function main(){
 	const api = await ApiPromise.create({ provider: wsProvider });
@@ -25,9 +47,12 @@ async function main(){
 		api.query.balances.freeBalance(ADDR)
 	])
 
-	console.log(genesisHash);
-	console.log(`The time is ${now}`);
-	console.log(`${ADDR} has ${balance.toNumber() / DOT_DECIMAL} KSM`);
+	let date_string = timeConverter(now.toNumber());
+	let human_balance = formatWithCommas(balance.toNumber() / DOT_DECIMAL);
+
+	console.log(`Genesis Hash: ${genesisHash}`);
+	console.log(`The time is ${date_string}`);
+	console.log(`${ADDR} has ${human_balance} KSM`);
 
 	process.exit(0);
 }
